2bd4022902185a32badbfe65ff2249f539b46bd6
Alexis Fexy <alexisfexy@gmail.com>
2022-04-09 12:41:07 -0700

    memoization and tabulation

--- data_structures/dynamic_programming/knapsack.py
+++ data_structures/dynamic_programming/knapsack.py
@@ -44,7 +44,57 @@ class Knapsack(object):
 
         return _helper(profits, weights, capacity, 0)
 
+    def dp_soln_memoization_recursive(self, profits: List[int], weights: List[int], capacity: int) -> int:
+        # Remove all duplicate calculations
+        # Store: [Capacity][Index]
+
+        def _helper(previous_calcs: List[List[int]], profits: List[int], weights: List[int], capacity: int,
+                    current_index: int):
+            if current_index >= len(weights) or capacity <= 0:
+                return 0
+
+            # Seen this calc, return it:
+            if previous_calcs[capacity][current_index] >= 0:
+                return previous_calcs[capacity][current_index]
+
+            include_profit: int = 0
+            weight: int = weights[current_index]
+            if weight <= capacity:
+                include_profit = profits[current_index] + _helper(previous_calcs, profits, weights, capacity - weight,
+                                                                  current_index + 1)
+
+            exclude_profit = _helper(previous_calcs, profits, weights, capacity, current_index + 1)
+
+            previous_calcs[capacity][current_index] = max(include_profit, exclude_profit)
+            return max(include_profit, exclude_profit)
+
+        previously_calculated = [[-1 for _ in range(capacity + 1)] for _ in range(len(profits))]
+        return _helper(previously_calculated, profits, weights, capacity, 0)
+
+    def dp_soln_tabulation(self, profits: List[int], weights: List[int], capacity: int) -> int:
+        tracker = [[0 for _ in range(capacity + 1)] for _ in range(len(profits))]
+        for item_index in range(len(profits)):
+            for cap in range(capacity + 1):
+                if item_index == 0:  # First item only
+                    tracker[item_index][cap] = profits[item_index] if weights[item_index] <= cap else 0
+                # Weight of item is greater than capacity
+                elif weights[item_index] > cap:
+                    tracker[item_index][cap] = tracker[item_index - 1][cap]
+                # Weight of item is equal or less than capacity:
+                # Either: profit from item + max of remaining capacity
+                # Or profit for capacity w/ one less item
+                else:
+                    remaining_cap = cap - weights[item_index]
+                    include_profit = tracker[item_index][remaining_cap] + profits[item_index]
+                    exclude_profit = tracker[item_index - 1][cap]
+                    tracker[item_index][cap] = max(include_profit, exclude_profit)
+        return tracker[len(profits) - 1][capacity]
+
+    def dp_soln_tabulation_2d(self, profits: List[int], weights: List[int], capacity: int) -> int:
+        pass
+
+
 
 if __name__ == "__main__":
-    max_profit = Knapsack().brute_force_recursive_soln([1, 6, 10, 16], [1, 2, 3, 5], 7)
+    max_profit = Knapsack().dp_solution_tabulation([1, 6, 10, 16], [1, 2, 3, 5], 7)
     print(max_profit)
